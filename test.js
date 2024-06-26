dc131f901f0b39b0c247b37635a280f3fe4eaede
djboi07 <dhans0703@gmail.com>
2024-06-26 15:38:29 +0530

    git commit

--- test.js
+++ test.js
@@ -1,28 +1,2 @@
 const gamebtn = document.getElementById('game1');
-let moving = false;
-
-gamebtn.addEventListener('mouseover', function() {
-    if (!moving) {
-        moving = true;
-        moveRight();
-    }
-});
-
-gamebtn.addEventListener('mouseout', function() {
-    moving = false;
-});
-
-function moveRight() {
-    if (moving) {
-        gamebtn.style.left = (gamebtn.offsetLeft + 1) + 'px';
-        gamebtn.style.top = (gamebtn.offsetTop + 1) + 'px';
-        requestAnimationFrame(moveRight);
-    }
-}
-
-
-document.getElementById('game1').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-    document.body.classList.toggle('light-mode');
-});
-
+console.log(gamebtn);
