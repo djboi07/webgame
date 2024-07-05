9f2a837bb0d2d7bb9d17d9389ab85602b4d419bc
djboi07 <dhans0703@gmail.com>
2024-07-05 13:04:06 +0530

    tictactoe with Controls

--- react/my-app/src/components/tictactoe/Tictactoe.js
+++ react/my-app/src/components/tictactoe/Tictactoe.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import Game from './Game';
 import './Tictactoe.css';
 
 function Tictactoe() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div className="Tictactoe">
       <h1>Tic Tac Toe</h1>
